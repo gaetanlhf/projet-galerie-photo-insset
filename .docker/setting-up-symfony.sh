adcd42282bf169fe828f343664050ef2d804a4cc
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-01 12:52:22 +0100

    Update setting-up-symfony.sh

--- .docker/setting-up-symfony.sh
+++ .docker/setting-up-symfony.sh
@@ -2,5 +2,6 @@
 cd ..
 DIRNAME=$(pwd)
 docker run --rm -v $(pwd):/opt/projet --name projet -u 1000:1000 php/symfony:projet symfony new skeleton --full
+shopt -s dotglob
 mv skeleton/* $DIRNAME
 rm -rf skeleton
