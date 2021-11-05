8e8c1f5e32f061ad6c1fa57bc9b0df6299eab2e0
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-05 20:09:50 +0100

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('partials/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
