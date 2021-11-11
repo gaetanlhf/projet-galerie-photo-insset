59c15f6da05484d4bbd0929768af64829fdb58dc
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-11 17:00:42 +0100

    Update UserAuthenticator.php

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -31,13 +31,13 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): PassportInterface
     {
-        $username = $request->request->get('username', '');
+        $email = $request->request->get('email');
 
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($username),
-            new PasswordCredentials($request->request->get('password', '')),
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
             ]
@@ -51,8 +51,8 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
