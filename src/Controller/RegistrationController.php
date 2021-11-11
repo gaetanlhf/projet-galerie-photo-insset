6a66ec2bf1f05adccb2dd7d1759d85500d3ca0af
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-11 13:11:39 +0100

    Working registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,34 +8,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class RegistrationController extends AbstractController
 {
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $registrationForm = $this->createForm(RegistrationFormType::class, $user);
+        $registrationForm->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-            $userPasswordHasherInterface->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
+        if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+
+            $user = $registrationForm->getData();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $this->genPwd()
             );
+
+            // encode the plain password
+            $user->setPassword($hashedPassword);
+            $user->setRoles(["USER"]);
+            $user->setIsEnabled(true);
+            $user->setNbLoginFailed("0");
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('app_home', array('success' => true));
         }
 
-        return $this->render('partials/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        $form_errors = array();
+
+        //this part get global form errors (like csrf token error)
+        foreach ($registrationForm->getErrors(true) as $error) {
+            $form_errors[] = $error->getMessage();
+        }
+
+
+        // dd($form_errors);
+        $this->addFlash('register_err', $form_errors);
+        return $this->redirectToRoute('app_home');
+    }
+
+    public function genPwd(): String
+    {
+        $length = "10";
+        $character = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $string = "";
+        $max = mb_strlen($character, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $string .= $character[random_int(0, $max)];
+        }
+        return $string;
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,46 +4,61 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Validator\Constraints\Regex;
+use App\Form\ReCaptchaType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('username', TextType::class, [
+                'required' => true,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                        'message' => 'Please enter an username',
+                    ])
                 ],
+                'attr' => [
+                    'placeholder' => 'Pseudo'
+                ]
             ])
-        ;
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email',
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Adresse de courriel'
+                ]
+            ])
+            ->add('age', IntegerType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an age',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[0-9]\d*$/',
+                        'message' => 'Âge incorrect.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Âge'
+                ]
+            ])
+            ->add('captcha', ReCaptchaType::class, [
+                'type' => 'checkbox', // (invisible, checkbox)
+                'mapped' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
