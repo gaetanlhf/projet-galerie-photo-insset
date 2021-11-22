6f015c60246a99dfd8ba03d131b63589ddc62a04
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-22 09:16:18 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findAllUserWithPublishedImages();
+        $randomUser = null;
+        if ($users != null) {
+            $randomUser = $this->getDoctrine()->getRepository(User::class)->find($users[mt_rand(0, count($users) - 1)]);
+        }
+
+        return $this->render('home/index.html.twig', ['randomUser' => $randomUser]);
     }
 }
