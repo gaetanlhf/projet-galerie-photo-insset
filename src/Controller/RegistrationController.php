5f1c30ef2bfdce4a162dea8b294b6c40ed479009
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-25 17:36:23 +0100

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,7 +32,10 @@ class RegistrationController extends AbstractController
             );
 
             $user->setPassword($hashedPassword);
+<<<<<<< Updated upstream
             $user->setRoles(["ROLE_USER"]);
+=======
+>>>>>>> Stashed changes
             $user->setIsEnabled(true);
             $user->setNbLoginFailed("0");
 
@@ -40,19 +43,43 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+<<<<<<< Updated upstream
             $email = (new TemplatedEmail())
                 ->from("noreply@dev.gaetanlhf.fr")
                 ->to(new Address($user->getEmail()))
                 ->subject("Votre mot de passe pour Galerie Photo")
+=======
+            if ($user->getId() == 1) {
+                $user->setRoles(["ROLE_ADMIN"]);
+            } else {
+                $user->setRoles(["ROLE_USER"]);
+            }
+            $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from("galeriephoto@gaetanlhf.fr")
+                ->to(new Address($user->getEmail()))
+                ->subject("Bienvenue sur Galerie Photo INSSET")
+>>>>>>> Stashed changes
                 ->htmlTemplate("emails/signup.html.twig")
                 ->context([
                     'username' => $user->getUsername(),
                     'password' => $password
                 ]);
 
+<<<<<<< Updated upstream
             // $mailer->send($email);
 
             $this->addFlash('register_suc', $password);
+=======
+            $mailer->send($email);
+            
+            if ($user->getRoles() == ["ROLE_ADMIN"]) {
+                $this->addFlash('register_suc', "Étant le premier utilisateur de cette instance, vous êtes désormais l'administrateur. Votre mot de passe vous été envoyé par courriel : " . $password);
+            } else {
+                $this->addFlash('register_suc', "Vous êtes désormais inscrit ! Votre mot de passe vous été envoyé par courriel : " . $password);
+            }
+>>>>>>> Stashed changes
 
             return $this->redirectToRoute('app_home');
         }
