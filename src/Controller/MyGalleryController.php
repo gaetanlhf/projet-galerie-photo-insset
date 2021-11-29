269505e7167b6d5afca9ac189473f4de27581b08
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-29 14:59:03 +0100

    Update MyGalleryController.php

--- src/Controller/MyGalleryController.php
+++ src/Controller/MyGalleryController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Image;
 use App\Form\ImageType;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,22 +14,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class MyGalleryController extends AbstractController
 {
     #[Route('/mygallery', name: 'app_mygallery')]
-    public function myGallery(): Response
+    public function myGallery(Request $request): Response
     {
+        $page = $request->query->get('page', 1);
         $setTitle = 'Ma galerie photo';
         $user = $this->getUser();
         $img = $this->getDoctrine()->getRepository(Image::class)->findAllImage($user);
         $countImg = $this->getDoctrine()->getRepository(Image::class)->countImage($user);
 
+        $pager = new Pagerfanta(new QueryAdapter($img));
+        $pager->setMaxPerPage(12);
+        if ($countImg < 12) {
+            $pager->setCurrentPage(1);
+        } else {
+            $pager->setCurrentPage($page);
+        }
+        $page = $pager->getCurrentPage();
+
         $image = new Image();
         $uploadForm = $this->createForm(ImageType::class, $image);
 
-        return $this->render('home/index.html.twig', ['uploadForm' => $uploadForm->createView(), 'setTitle' => $setTitle, 'img' => $img, 'countImg' => $countImg]);
+        return $this->render('home/index.html.twig', ['uploadForm' => $uploadForm->createView(), 'setTitle' => $setTitle, 'images' => $pager, 'page' => $page, 'countImg' => $countImg]);
     }
 
     #[Route('/mygallery/upload', name: 'app_mygallery_upload')]
     public function myGalleryUpload(Request $request): Response
     {
+
+        $page = $request->query->get('page', 1);
+
         $user = $this->getUser();
 
         $image = new Image();
@@ -53,7 +68,7 @@ class MyGalleryController extends AbstractController
             $em->flush();
 
             $user->addImage($image);
-
+            
             $this->addFlash('gallery_suc', 'Image ajoutée avec succès.');
 
         } else {
@@ -66,12 +81,14 @@ class MyGalleryController extends AbstractController
 
             $this->addFlash('gallery_upload_err', $form_errors);
         }
-        return $this->redirect($this->generateUrl('app_mygallery'));
+        return $this->redirectToRoute('app_mygallery', ['page' => $page]);   
     }
 
     #[Route('/mygallery/changepos', name: 'app_mygallery_changepos')]
     public function myGalleryChangePos(Request $request): Response
     {
+        $page = $request->query->get('page', 1);
+
         $image = $request->query->get('image');
         $position = $request->query->get('position');
         $user = $this->getUser();
@@ -81,7 +98,6 @@ class MyGalleryController extends AbstractController
             if ($image) {
                 if ($position == $image[0]->getPosition()) {
                     $this->addFlash('gallery_err', 'L\'image est déjà sur cette position.');
-                    return $this->redirectToRoute('app_mygallery');
                 } else if ($position != 0 && $entityManager->getRepository(Image::class)->findBy(['user' => $user, 'position' => $position])) {
                     $userImgs = $entityManager->getRepository(Image::class)->findBy(['user' => $user]);
                     foreach ($userImgs as $img) {
@@ -101,6 +117,7 @@ class MyGalleryController extends AbstractController
                     $image[0]->setPosition($position);
                     $entityManager->flush();
                     $this->addFlash('gallery_suc', 'L\'image a bien été placée en position ' . $position . ' avec succès !');
+                    $page = ceil($position/12);
                 } else {
                     if ($position == 0) {
                         $userImgs = $entityManager->getRepository(Image::class)->findBy(['user' => $user]);
@@ -114,25 +131,33 @@ class MyGalleryController extends AbstractController
                             }
                         }
                         $this->addFlash('gallery_suc', 'L\'image a bien été dépubliée avec succès !');
+                        $page = 1;
                     } else {
                         $this->addFlash('gallery_suc', 'L\'image a bien été placée en position ' . $position . ' avec succès !');
+                        $page = ceil($position/12);
                     }
                     $image[0]->setPosition($position);
                     $entityManager->flush();
                 }
             }
-            return $this->redirectToRoute('app_mygallery');
         } else {
             $this->addFlash('gallery_err', 'Impossible de modifier la position d\'une image inexistante.');
-            return $this->redirectToRoute('app_mygallery');
         }
+        return $this->redirectToRoute('app_mygallery', ['page' => $page]);   
     }
 
     #[Route('/mygallery/delete', name: 'app_mygallery_delete')]
     public function myGalleryDelete(Request $request): Response
     {
+        $page = $request->query->get('page', 1);
+
         $image = $request->query->get('image');
         $user = $this->getUser();
+        $countImg = $this->getDoctrine()->getRepository(Image::class)->countImage($user);
+
+        if ($countImg < 12) {
+            $page = 1;
+        }
 
         if ($image) {
             $entityManager = $this->getDoctrine()->getManager();
@@ -156,10 +181,10 @@ class MyGalleryController extends AbstractController
                 $entityManager->flush();
                 $this->addFlash('gallery_suc', 'Image supprimée avec succès.');
             }
-            return $this->redirectToRoute('app_mygallery');
         } else {
             $this->addFlash('gallery_err', 'Impossible de supprimer une image inexistante.');
-            return $this->redirectToRoute('app_mygallery');
         }
+        return $this->redirectToRoute('app_mygallery', ['page' => $page]);   
+
     }
 }
