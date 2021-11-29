4d3f749b5b9e6a46f998d70fb71f4d65d7289ea5
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-11-29 15:20:25 +0100

    Cleaning

--- migrations/Version20211129141811.php
+++ migrations/Version20211129141811.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211112204246 extends AbstractMigration
+final class Version20211129141811 extends AbstractMigration
 {
     public function getDescription(): string
     {
