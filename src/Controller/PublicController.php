a8af5984b986be07a8168dbf4477064406245672
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-12-01 22:23:13 +0100

    Merging controllers

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -7,12 +7,29 @@ use App\Entity\User;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class GalleryController extends AbstractController
+class PublicController extends AbstractController
 {
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAllUserWithPublishedImages();
+        $randomUser = null;
+        $pager = null;
+        if ($users != null) {
+            $randomUser = $this->getDoctrine()->getRepository(User::class)->find($users[mt_rand(0, count($users) - 1)]);
+            $randomUserImg = $this->getDoctrine()->getRepository(Image::class)->findImagePublished($randomUser);
+            $pager = new Pagerfanta(new QueryAdapter($randomUserImg));
+            $pager->setMaxPerPage(12);
+            $pager->setCurrentPage(1);
+        }
+
+        return $this->render('home/index.html.twig', ['randomUser' => $randomUser, 'randomUserImg' => $pager]);
+    }
+
     #[Route('/u/{username}', name: 'app_usergallery')]
     public function userGallery(Request $request, string $username): Response
     {
@@ -38,7 +55,7 @@ class GalleryController extends AbstractController
         return $this->render('home/index.html.twig', ['username' => $username, 'setTitle' => $setTitle, 'images' => $pager]);
     }
 
-    public function userList()
+    public function userList(): Response
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAllUserWithPublishedImages();
         if ($users != null) {
