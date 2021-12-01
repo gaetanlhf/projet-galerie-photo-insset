5aeee63d931f7e880f7c2d0c1fd85865b9ec37bc
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-12-01 22:23:43 +0100

    Update LoginController.php

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,9 +11,7 @@ class LoginController extends AbstractController
 {
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('partials/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
